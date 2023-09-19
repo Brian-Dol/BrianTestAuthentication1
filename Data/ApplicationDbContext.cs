485c16c64422e4e2483ee60270358447a4814edf
sheena <sheena@LAPTOP-MIAOL2R3>
2023-09-19 19:30:05 +0400

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using BrianTestAuthentication1.Models;
+
+namespace BrianTestAuthentication1.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<BrianTestAuthentication1.Models.Joke>? Joke { get; set; }
+    }
+}
